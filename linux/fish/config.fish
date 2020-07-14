8fcbd8cccc3d15b90c35a980fa69cb694a09ecb2
shanghai_yakisoba=\chan! <koumaza--@outlook.com>
2020-07-15 02:43:54 +0900

    Update config.fish

--- linux/fish/config.fish
+++ linux/fish/config.fish
@@ -21,6 +21,12 @@ alias m="mkdir -p"
 function cm; mkdir -p $argv ; cd $argv ; end
 alias cf="env HOME=(mktemp -d) fish --login"
 alias curlo="curl -LO"
+# Fisher
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
 # Prompt
 function fish_prompt
         set -gx pstatus $status
