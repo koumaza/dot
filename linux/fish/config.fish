c20052b9612771683f377bee592aff6ecc403df6
shanghai_yakisoba=\chan! <koumaza--@outlook.com>
2020-07-15 02:29:21 +0900

    Update config.fish

--- linux/fish/config.fish
+++ linux/fish/config.fish
@@ -9,7 +9,7 @@ alias fish.=". ~/.config/fish/config.fish"
 alias repo2sync="repo sync -c -j(math (nproc --all) \* 24) --force-sync --no-clone-bundle --no-tags"
 function osstinpois;    echo 'mkdir -p ~/log/$(pwd|tr / _|sed -E s/^_//) && mka bacon -j$(($(nproc --all) * 24)) | tee ~/log/$(pwd|tr '/' '_'|sed -E 's/^_//')/$(date '+%Z_%m%d-%H:%M:%S.%N'.log)'; end
 function asshole; while true; echo "Syncing..."; sudo nice -n18 sync; echo "Sync completed successfully"; echo "1 \> drop_caches"; echo Dropping: [(echo -ne 1 |sudo nice -n18 tee /proc/sys/vm/drop_caches)]; echo "Drop-caches completed successfully"; sleep 10; echo "Syncing..."; sudo nice -n18 sync; echo "Sync completed successfully";: '#echo "2 \> drop_caches" #echo Dropping: [(echo -ne 2 |sudo nice -n18 tee /proc/sys/vm/drop_caches)] #echo "Drop-caches completed successfully" #sleep 10'; end; end
-function updatefishconfig; echo (curl -L "https://raw.githubusercontent.com/koumaza/dot/default/linux/fish/config.fish" | tee ~/.config/fish/config.fish) 1>/dev/null; end
+function updatefishconfig; curl -Ls https://raw.githubusercontent.com/koumaza/dot/default/linux/fish/config.fish | tee ~/.config/fish/config.fish >/dev/null; end
 alias l="ls"
 alias b="bash -c"
 alias v="vim"
